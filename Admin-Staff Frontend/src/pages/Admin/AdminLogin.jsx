9930adb558f5660e5a88c4149eeeb47f8a31dac9
Maria Turano <turano.educ@gmail.com>
2026-05-24 01:26:12 +0800

    UPDATE: Added reCAPTCHA

--- Admin-Staff Frontend/src/pages/Admin/AdminLogin.jsx
+++ Admin-Staff Frontend/src/pages/Admin/AdminLogin.jsx
@@ -167,7 +167,7 @@ export default function AdminLogin() {
             <div className="flex justify-center mt-4">
               <ReCAPTCHA
                 ref={recaptchaRef}
-                sitekey={import.meta.env.RECAPTCHA_SITE_KEY}
+                sitekey={import.meta.env.VITE_RECAPTCHA_SITE_KEY}
                 onChange={(token) => setRecaptchaToken(token)}
                 onExpired={() => setRecaptchaToken(null)}
               />
