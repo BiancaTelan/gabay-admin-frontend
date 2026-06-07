f08044a4a5c2506ddedf58aa2994b49f0e264e4b
trixsh99 <trixiabautista09@gmail.com>
2026-06-07 14:27:22 +0800

    ..

--- Admin-Staff Frontend/src/pages/Staff/StaffAppointments.jsx
+++ Admin-Staff Frontend/src/pages/Staff/StaffAppointments.jsx
@@ -435,8 +435,8 @@ export default function StaffAppointments() {
               <Search className="absolute right-3 top-2.5 text-gray-400" size={18} />
             </div>
             <div className="flex gap-2">
-              <Button variant="teal-outline" onClick={openFilter} className="px-10 py-2">
-                <Funnel size={16} /> Filter & Sort
+              <Button variant="teal-outline" onClick={openFilter} className="px-10 py-2 min-w-[180px]">
+                <Funnel size={16} className="mr-2" /> Filter & Sort
               </Button>
               <Button variant="teal" onClick={exportToExcel} className="py-2 px-8 min-w-[180px]">
                 <Download size={16} className="inline mr-2" /> Export Excel
